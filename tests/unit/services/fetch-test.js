f7367e7fc7a244ff874a287daa66aff017e9ba90
Lux <lux@lux.name>
2019-07-11 20:48:21 +0200

    test for overwrite

--- tests/unit/services/fetch-test.js
+++ tests/unit/services/fetch-test.js
@@ -84,4 +84,23 @@ module('Unit | Service | fetch', function(hooks) {
 
     assert.equal(req.status, 200, 'Status code is ok');
   });
+
+  test('it correctly overwrites a header', async function(assert) {
+    let service = this.owner.lookup('service:fetch');
+    service.set('headers', {
+      'a-header': 'one',
+    });
+
+    server.get('/network', request => [
+      request.requestHeaders['a-header'] === 'two' ? 200 : 500,
+      {},
+      ''
+    ]);
+
+    const req = await service.fetch('/network', {
+      headers: { 'a-header': 'two' },
+    });
+
+    assert.equal(req.status, 200, 'Status code is ok')
+  });
 });
